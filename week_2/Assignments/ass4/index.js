5dbead861f0d3ddc7c3ab9a67d6cea3238624385
Mahmoud Shehata <mmshehatta@gmail.com>
2022-11-09 23:52:18 +0200

    `Solving` Ass4.

---
+++ week_2/Assignments/ass4/index.js
@@ -0,0 +1,9 @@
+// التكليف 04
+// ========
+function yesOrNo(val) {
+    return val > 10;
+}
+// Do Not Edit Here
+//   console.log(yesOrNo("100")); // Error
+console.log(yesOrNo(30)); // True
+console.log(yesOrNo(8)); // False
---
+++ week_2/Assignments/ass4/index.ts
@@ -0,0 +1,11 @@
+// التكليف 04
+// ========
+
+function yesOrNo(val: number | boolean) : true| false {
+    return val > 10;
+  }
+  
+  // Do Not Edit Here
+//   console.log(yesOrNo("100")); // Error
+  console.log(yesOrNo(30)); // True
+  console.log(yesOrNo(8)); // False
