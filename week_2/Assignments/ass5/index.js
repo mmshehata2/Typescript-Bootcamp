a6e78c6c26f5075fc195cdca6a54960df92ac0cf
Mahmoud Shehata <mmshehatta@gmail.com>
2022-11-11 03:46:02 +0200

    `Solving` Ass5.

---
+++ week_2/Assignments/ass5/index.js
@@ -0,0 +1,8 @@
+function isHeOld(age) {
+    // return age > 40;
+    return age > 40 ? "Yes" : "No";
+}
+// Do Not Edit Here
+//   console.log(isHeOld("100")); // Error
+console.log(isHeOld(45)); // "Yes"
+console.log(isHeOld(30)); // "No"
