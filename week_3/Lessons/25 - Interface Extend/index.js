ea7a7a58b83ebcbd095a611adf2f8b854f605e72
Mahmoud Shehata <mmshehatta@gmail.com>
2022-11-25 03:15:56 +0200

    `Finishing` Lesson 25.

---
+++ week_3/Lessons/25 - Interface Extend/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+  Interface
+  - Extending Interfaces
+*/
+exports.__esModule = true;
+var user = {
+    id: 100,
+    username: "Elzero",
+    country: "Egypt",
+    role: "Mod",
+    protect: true
+};
+console.log(user.id);
---
+++ week_3/Lessons/25 - Interface Extend/index.ts
@@ -0,0 +1,30 @@
+/*
+  Interface
+  - Extending Interfaces
+*/
+
+interface User {
+    id: number;
+    username: string;
+    country: string;
+  }
+  
+  interface Moderator {
+    role: string | number;
+  }
+  
+  interface Admin extends User,Moderator {
+    protect?: boolean;
+  }
+  
+  let user: Admin = {
+    id: 100,
+    username: "Elzero",
+    country: "Egypt",
+    role: "Mod",
+    protect: true
+  }
+  
+console.log(user.id);
+  
+export{}
