4b3bd52a02eb44268879a644d251015ef17f76eb
Mahmoud Shehata <mmshehatta@gmail.com>
2022-11-25 03:00:28 +0200

    `Finishing` Lesson 24.

---
+++ week_3/Lessons/24 – Interface ReOpen And Use Cases/index.ts
@@ -0,0 +1,27 @@
+/*
+  Interface
+  - ReOpen The Interface And Use Cases
+*/
+
+// Homepage
+interface Settings {
+    readonly theme: boolean;
+    font: string;
+  }
+  
+  // Articles Page
+  interface Settings {
+    sidebar: boolean;
+  }
+  
+  // Contact Page
+  interface Settings {
+    external: boolean;
+  }
+  
+  let userSettings: Settings = {
+    theme: true,
+    font: "Open Sans",
+    sidebar: false,
+    external: true
+  }
